ff01edf98d77598f66fab35b6b3dc068e6c3aab0
Zazhil Adhafi <zazhil24@gmail.com>
2023-07-04 21:50:45 +0700

    Init project. Register user endpoint

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bwastartup/handler"
+	"bwastartup/user"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+	userHandler := handler.NewUserHandler(userService)
+
+	router := gin.Default()
+	api := router.Group("/api/v1")
+
+	api.POST("/users", userHandler.RegisterUser)
+
+	router.Run()
+
+	// userInput := user.RegisterUserInput{}
+	// userInput.Name = "Tes simpan dari service"
+	// userInput.Email = "contoh@gmail.com"
+	// userInput.Occupation = "karyawan"
+	// userInput.Password = "password"
+
+	// userService.RegisterUSer(userInput)
+	// user := user.User{
+	// 	Name: "Test simpan",
+	// }
+
+	// userRepository.Save(user)
+
+	// router := gin.Default()
+	// router.GET("/handler", handler)
+	// router.Run()
+
+}
+
+// func handler(c *gin.Context) {
+// 	dsn := "root:@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+// 	if err != nil {
+// 		log.Fatal(err.Error())
+// 	}
+
+// 	var users []user.User
+// 	db.Find(&users)
+
+// 	c.JSON(http.StatusOK, users)
+// }
