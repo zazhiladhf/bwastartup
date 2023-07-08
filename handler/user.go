488dc72d84eac376fe96f5bb423ae4851bd80192
Zazhil Adhafi <zazhil24@gmail.com>
2023-07-08 19:06:57 +0700

    add authMiddleware

--- handler/user.go
+++ handler/user.go
@@ -150,7 +150,8 @@ func (h *userHandler) UploadAvatar(c *gin.Context) {
 		return
 	}
 
-	userID := 3
+	currentUser := c.MustGet("currentUser").(user.User)
+	userID := currentUser.ID
 	path := fmt.Sprintf("images/%d-%s", userID, file.Filename)
 	err = c.SaveUploadedFile(file, path)
 	if err != nil {
